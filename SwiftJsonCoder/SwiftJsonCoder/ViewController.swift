67d0c4b9002818527e220a2b2c2d9961d052d036
来高强 <laigaoqiang@jiayuan.com>
2018-01-08 10:40:41 +0800

    create project SwiftJsonCoder

---
+++ SwiftJsonCoder/SwiftJsonCoder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftJsonCoder
+//
+//  Created by 来高强 on 2018/1/8.
+//  Copyright © 2018年 Ligo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftJsonCoder/SwiftJsonCoderTests/SwiftJsonCoderTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftJsonCoderTests.swift
+//  SwiftJsonCoderTests
+//
+//  Created by 来高强 on 2018/1/8.
+//  Copyright © 2018年 Ligo. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftJsonCoder
+
+class SwiftJsonCoderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
