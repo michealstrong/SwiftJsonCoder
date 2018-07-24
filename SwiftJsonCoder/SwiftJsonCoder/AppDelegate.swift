dd2321fb36a6faa849d45e71fe696b9861a982f4
laigaoqiang <laigaoqiang@163.com>
2018-07-24 15:47:16 +0800

    添加打印

--- SwiftJsonCoder/SwiftJsonCoder/AppDelegate.swift
+++ SwiftJsonCoder/SwiftJsonCoder/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let str = "125"
+        print(str)
         return true
     }
 
